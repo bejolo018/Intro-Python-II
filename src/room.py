f64a96211134047813e42b1a2750b7dd8c5d133e
bejolo018 <bejolo018@gmail.com>
2019-09-06 02:21:11 -0500

    created class components, still receiving some bugs, needs more work

--- src/room.py
+++ src/room.py
@@ -1,4 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
-class Room
+class Room:
+    def __init__ (name, description):
+        self.name = name
+        self.description = description
+
+def __str__(self):
+    return f"Room: {self.name}, {self.description}"
+
+def _repr__(self):
+    return f"Room({repr(self.name)}, {repr(self.description)})"
